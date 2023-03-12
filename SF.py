b3c47a3c8fba75880dbf012655c3cf9cd1a6c69e
vladshulakov <iamrock@yandex.ru>
2023-03-12 19:40:26 +0300

    SF

---
+++ SF.py
@@ -0,0 +1,71 @@
+game_field = ([' ', 0, 1, 2], [0, '-', '-', '-'], [1, '-', '-', '-'], [2, '-', '-', '-'])
+
+def pr_gf():
+    for i in range(len(game_field)):
+        print(*game_field[i])
+
+
+def coords(x,y, sign):
+    global game_field
+    game_field[int(y) + 1][int(x) + 1] = sign
+
+def sign():
+    i = 0
+    while True:
+        yield 'x' if i % 2 == 0 else 'o'
+        i += 1
+
+def game_over(cur_s):
+    if all(i == cur_s for i in game_field[1][1:]):
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif all(i == cur_s for i in game_field[2][1:]):
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif all(i == cur_s for i in game_field[3][1:]):
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif game_field[1][1] == cur_s and game_field[2][2] == cur_s and game_field[3][3] == cur_s:
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif game_field[3][1] == cur_s and game_field[2][2] == cur_s and game_field[1][3] == cur_s:
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif game_field[1][1] == cur_s and game_field[2][1] == cur_s and game_field[3][1] == cur_s:
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif game_field[1][2] == cur_s and game_field[2][2] == cur_s and game_field[3][2] == cur_s:
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif game_field[1][3] == cur_s and game_field[2][3] == cur_s and game_field[3][3] == cur_s:
+        print(f'Game over, победили {cur_s}')
+        exit()
+    elif all(i != '-' for i in game_field[1][1:]) and all(i != '-' for i in game_field[2][1:]) and all(i != '-' for i in game_field[3][1:]):
+        print('Game over, победителя нет!')
+        exit()
+
+a = sign()
+current_sign = next(a)
+turn = 0
+pr_gf()
+
+while  True:
+    
+
+    while (x := input(f'Ходит {current_sign} введите координату по Х от 0 до 2: ')) not in ['0', '1', '2']:
+        print('Неподходящая координата')
+    while (y := input(f'Ходит {current_sign} введите координату по Y от 0 до 2: ')) not in ['0', '1', '2']:
+        print('Неподходящая координата')
+    if game_field[int(y) + 1][int(x) + 1] != '-':
+        print('Данная координата уже занята, введите другое значение')
+        continue
+
+    coords(x, y, current_sign)
+    pr_gf()
+    
+    game_over(current_sign)
+
+
+
+
+    current_sign = next(a)
